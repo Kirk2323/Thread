a28c3bbce4e8563cfae93fc77d2f405333223b68
Kirk <94045830+Kirk2323@users.noreply.github.com>
2022-11-21 11:11:18 +0100

    Add files via upload

--- Sockets/client.py
+++ Sockets/client.py
@@ -1,9 +1,53 @@
 import socket
 
-host,port =('10.128.2.27', 2048)
+host,port =('127.0.0.1', 2049)
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((host, port))
+client_socket = socket.socket()
+client_socket.connect(("localhost", port))
+#client_socket.connect((host, port))
 
-data =client_socket.recv(1024).decode()
+#message =client_socket.recv(1024).decode()
+
+#ce qui est reçu
+msg =""
+#ce qui est envoyé
+data=""
+#while msg !="exit" and msg !="bye" and data!="bye" and data!="exit":
+#    msg = input("-->")
+#    client_socket.send(msg.encode())
+#    client_socket.recv(1024).decode()
+#    print(data)
+#client_socket.close()
+
+
 print("Le client vient de se connecter")
+
+a = int(input("Voulez vous :"
+              "\n1. Fermer la connexion"
+              "\n2. Envoyer un message \n"))
+
+if a == 1:
+    client_socket.close()
+    print("La connexion vient d'être fermée")
+
+if a == 2:
+    print("Vous pouvez désormais envoyer un message")
+    while msg != "exit" and msg != "bye" and data != "bye" and data != "exit":
+        msg = input("-->")
+        client_socket.send(msg.encode())
+        data = client_socket.recv(1024).decode()
+        print(data)
+    client_socket.close()
+
+
+while a != 1 and a!= 2 :
+    a = int(input("Merci de choisir un chiffre valable (1 ou 2) \n"))
+    if a == 1:
+        print("La connexion vient d'être fermée")
+    if a == 2:
+        print("Vous pouvez désormais envoyer un message")
+
+
+
+
+
