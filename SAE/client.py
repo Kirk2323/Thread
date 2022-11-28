cc8a3d7aeec498ff8da66532481a520b8a177d80
Kirk <94045830+Kirk2323@users.noreply.github.com>
2022-11-28 21:56:16 +0100

    Add files via upload

--- SAE/client.py
+++ SAE/client.py
@@ -1,6 +1,6 @@
 import socket
-
-host,port =('127.0.0.1', 2049)
+import os
+host,port =('127.0.0.1', 2047)
 
 client_socket = socket.socket()
 client_socket.connect(("localhost", port))
@@ -9,13 +9,15 @@ msg =""
 #ce qui est envoyé
 data=""
 
-
+#liste pour manier les OS
+w = []
 
 print("Le client vient de se connecter")
 
 a = int(input("Voulez vous :"
               "\n1. Fermer la connexion"
-              "\n2. Envoyer un message \n"))
+              "\n2. Envoyer un message "
+              "\n3. Connaître l'os du serveur"))
 
 if a == 1:
     client_socket.close()
@@ -30,30 +32,35 @@ if a == 2:
         print(data)
     client_socket.close()
 
+if a == 3:
+    r = os.name
+    w.append(r)
+    if "nt" in w:
+        print("L'os du serveur est Windows.")
+        #permet de retirer l'os de la liste au cas où l'utilisateur change d'os entre temps
+        w.pop(0)
+    if "posix" in w:
+        print("L'os du serveur est Linux.")
+        # permet de retirer l'os de la liste au cas où l'utilisateur change d'os entre temps
+        w.pop(0)
 
-while a != 1 and a!= 2 :
+while a != 1 and a!= 2 and a!=3 :
     a = int(input("Merci de choisir un chiffre valable (1 ou 2) \n"))
     if a == 1:
         print("La connexion vient d'être fermée")
     if a == 2:
         print("Vous pouvez désormais envoyer un message")
-
-
-
-
-#Projet mené en BUT1 l'année dernière
-
-
-#client_socket.connect((host, port))
-
-#message =client_socket.recv(1024).decode()
-
-#while msg !="exit" and msg !="bye" and data!="bye" and data!="exit":
-#    msg = input("-->")
-#    client_socket.send(msg.encode())
-#    client_socket.recv(1024).decode()
-#    print(data)
-#client_socket.close()
+    if a == 3:
+        r = os.name
+        w.append(r)
+        if "nt" in w:
+            print("L'os du serveur est Windows.")
+            # permet de retirer l'os de la liste au cas où l'utilisateur change d'os entre temps
+            w.pop(0)
+        if "posix" in w:
+            print("L'os du serveur est Linux.")
+            # permet de retirer l'os de la liste au cas où l'utilisateur change d'os entre temps
+            w.pop(0)
 
 
 
