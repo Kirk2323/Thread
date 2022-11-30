6b835a3aadee6b638029269c46fd3fb09a3a63b4
Kirk <94045830+Kirk2323@users.noreply.github.com>
2022-11-30 22:23:55 +0100

    Add files via upload

--- SAE/client.py
+++ SAE/client.py
@@ -3,7 +3,7 @@ import os
 import psutil
 import platform
 
-host,port =('127.0.0.1', 2047)
+host,port =('127.0.0.1', 2089)
 
 client_socket = socket.socket()
 client_socket.connect(("localhost", port))
@@ -17,66 +17,24 @@ w = []
 print("Le client vient de se connecter")
 a=0
 
-while a!=1:
-    a = int(input("Voulez vous :"
-                  "\n1. Fermer la connexion : "
-                  "\n2. Avoir l'adresse IP locale :"
-                  "\n3. Connaître l'os :"
-                  "\n4. Obtenir l'usage du CPU :"
-                  "\n5. Avoir le nom du poste :"
-                  "\n6. Obtenir la quantité de RAM :"))
-    if a == 1:
-        print("\nLa connexion vient d'être fermée")
+while msg != "1" and data != "1" and msg != "7" and data != "7":
+    msg = input("Voulez vous :"
+                   "\n1. Fermer la connexion : "
+                   "\n2. Avoir l'adresse IP locale :"
+                   "\n3. Connaître l'os :"
+                   "\n4. Obtenir l'usage du CPU :"
+                   "\n5. Avoir le nom du poste :"
+                   "\n6. Obtenir la quantité de RAM :")
 
-    if a == 2:
-        print(f"\nVoici l'adresse IP du pc : {socket.gethostbyname(socket.gethostname())}\n")
+    if msg == "1":
+        print("Vous allez être déconnecté")
+        # Ajouter un moyen de se déco
+    else:
+        client_socket.send(msg.encode())
+    data = client_socket.recv(1024).decode()
+    print(data)
 
-    if a == 3:
-        print(f"\nNom de l'os :", platform.system())
-        print(f"Version du système :{platform.release()}\n")
-
-    if a == 4:
-        print(f'\nL"usage du CPU est : {psutil.cpu_percent(4)}\n')
-
-    if a==5:
-        print(f"\nLe nom du pc est : {socket.gethostname()} \n" )
-
-    if a ==6:
-        # Getting % usage of virtual_memory ( 3rd field)
-        print(f'\nMémoire RAM utilisé: {psutil.virtual_memory()[2]} %')
-        # Getting usage of virtual_memory in GB ( 4th field)
-        vm = round(psutil.virtual_memory()[3] / 1000000000,2)
-        print(f'RAM Utilisé (GB): {vm}\n')
-
-
-
-while a != 1 and a!= 2 and a!=3 and a!=4 and a!=5 :
-    a = int(input("Merci de choisir un chiffre valable (1 ou 2) \n"))
-    if a == 1:
-        print("La connexion vient d'être fermée")
-        socket.connect.close()
-    if a == 2:
-        print(f"Voici l'adresse IP du pc : {socket.gethostbyname(socket.gethostname())}\n")
-    if a == 3:
-        print(f"\nNom de l'os :", platform.system())
-        print(f"Version du système :{platform.release()}\n")
-    if a == 4:
-        print(f'\nL"usage du CPU est : {psutil.cpu_percent(3)}\n')
-
-    if a==5:
-        print(f"\nLe nom du pc est : {socket.gethostname()} \n" )
-
-    if a ==6:
-        # Getting % usage of virtual_memory ( 3rd field)
-        print(f'\nMémoire RAM utilisé: {psutil.virtual_memory()[2]} %')
-        # Getting usage of virtual_memory in GB ( 4th field)
-        vm = round(psutil.virtual_memory()[3] / 1000000000,2)
-        print(f'RAM Utilisé (GB): {vm}\n')
-
-
-
-
-
+client_socket.close()
 
 
 
--- SAE/serveur.py
+++ SAE/serveur.py
@@ -1,8 +1,10 @@
 import socket
 import os
 import sys
+import psutil
+import platform
 
-host,port =('127.0.0.1', 2047)
+host,port =('127.0.0.1', 2089)
 print("Le serveur se lance")
 
 server_socket = socket.socket()
@@ -11,26 +13,39 @@ server_socket.listen(1)
 
 print(f"Le serveur {host} est en attente de connexion")
 
-while True:
-    conn,address = server_socket.accept()
-    msg=""
-    data=""
-    while msg != "exit" and msg != "bye" and data != "bye" and data != "exit":
+
+
+
+
+
+while True :
+    conn, address = server_socket.accept()
+    msg = ""
+    data = ""
+
+    while msg != "1" and data != "1" and msg != "7" and data != "7":
         data = conn.recv(1024).decode()
         print(data)
-        msg=input("serveur -->")
-        conn.send(msg.encode())
-    rep = input("continuer (y/n)")
-    if rep =="n":
-        break
-
-server_socket.close()
-
-
-
-
-
-
-
-
+        # if data=="1":
+            #FAIRE UN MOYEN DE SE DECONNECTER
+        if data == "2" :
+            message = f"\nVoici l'adresse IP du pc : {socket.gethostbyname(socket.gethostname())}\n"
+            conn.send(message.encode())
+        elif data == "3":
+            message = f"\nNom de l'os : {platform.system()} | Version du système : {platform.release()}\n"
+            conn.send(message.encode())
+        elif data == "4":
+            message = f'\nL"usage du CPU est : {psutil.cpu_percent(4)}\n'
+            conn.send(message.encode())
+        elif data == "5":
+            message = f"\nLe nom du pc est : {socket.gethostname()} \n"
+            conn.send(message.encode())
+        elif data =='6':
+            vm = round(psutil.virtual_memory()[3] / 1000000000, 2)
+            message = f'\nMémoire RAM utilisé: {psutil.virtual_memory()[2]} % | RAM Utilisé (GB): {vm}\n'
+            conn.send(message.encode())
+        else:
+            msg='Votre commande n"existe pas, merci de rentrer un chiffre valide.'
+            conn.send(msg.encode())
+    conn.close()
 
